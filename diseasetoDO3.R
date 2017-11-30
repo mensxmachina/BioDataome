d64db35c7fa52e2135c2142563a739da4b1e604a
kliolak <kliolak@gmail.com>
2017-11-30 11:07:33 +0200

    Publish

--- diseasetoDO3.R
+++
@@ -1,7 +0,0 @@
-#map a disease to DO-level2
-
-diseasetoDO3<-function(x){
-  diseaseSubCategoryALLU<-get(load("E:/Dropbox (MXM - UoC)/Documents/Data-Based IR/Paper Experiments/Annotation/diseaseSubCategoryALLU.Rda"))
-  do3<-diseaseSubCategoryALLU[match(tolower(x),tolower(diseaseSubCategoryALLU[,1])),4]
-return(do3)
-}
